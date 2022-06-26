7e25acbb538486a76a8a5bebed319a9554797395
victor90braz <victor.90braz@gmail.com>
2022-06-26 11:36:30 +0200

    Export IPerfil and perfilDefault

--- src/components/MainComponent/MainComponent.tsx
+++ src/components/MainComponent/MainComponent.tsx
@@ -1,55 +1,47 @@
 import { useState } from "react";
 import { SiHomebridge } from "react-icons/si";
 import perfil from "../../images/perfil.jpg";
+import { IPerfil } from "../../Interfaces/IPerfil";
+import { perfilDefault } from "../../utiles/utiles";
 import MainComponentStyled from "./MainComponentStyled";
 
 const MainComponent = () => {
-  const [datas] = useState([
-    {
-      keywords: "Experienced",
-      name: "Kimberly Hill",
-      like: 10,
-      point: 170,
-      rate: 53,
-      game: 25,
-    },
-  ]);
+  const [datas] = useState<IPerfil[]>(perfilDefault);
 
   return (
     <MainComponentStyled>
-      <ul>
-        {datas.map((data, index) => {
-          return (
-            <li key={index}>
-              <div className="element-perfil">
-                <img src={perfil} alt={`Perfil atleta`} />
-              </div>
-              <div className="element-data">
-                <span>{data.keywords}</span>
-                <h1>{data.name}</h1>
-                <span>{data.like} Likes</span>
-              </div>
+      {datas.map((data, index) => {
+        return (
+          <div key={index}>
+            <div className="element-perfil">
+              <img src={perfil} alt={`Perfil atleta`} />
+            </div>
 
-              <ul className="element-statistics">
-                <li className="points">
-                  <SiHomebridge />
-                  <h3>Points</h3>
-                  <span>{data.point}</span>
-                </li>
-                <li className="rate">
-                  <span>{data.rate}</span>
-                  <span>Win Lose Ratio</span>
-                </li>
-                <li className="game">
-                  <SiHomebridge />
-                  <h3>Games Won</h3>
-                  <span>{data.game}</span>
-                </li>
-              </ul>
-            </li>
-          );
-        })}
-      </ul>
+            <div className="element-data">
+              <span>{data.keywords}</span>
+              <h1>{data.name}</h1>
+              <span>{data.like} Likes</span>
+            </div>
+
+            <ul className="element-statistics">
+              <li className="points">
+                <SiHomebridge />
+                <h3>Points</h3>
+                <span>{data.point}</span>
+              </li>
+              <li className="rate">
+                <span>{data.rate}</span>
+                <span>Win Lose Ratio</span>
+              </li>
+              <li className="game">
+                <SiHomebridge />
+                <h3>Games Won</h3>
+                <span>{data.game}</span>
+              </li>
+            </ul>
+          </div>
+        );
+      })}
     </MainComponentStyled>
   );
 };
---
+++ src/utiles/utiles.tsx
@@ -0,0 +1,10 @@
+export const perfilDefault = [
+  {
+    keywords: "Experienced",
+    name: "Kimberly Hill",
+    like: 12,
+    point: 170,
+    rate: 53,
+    game: 25,
+  },
+];
