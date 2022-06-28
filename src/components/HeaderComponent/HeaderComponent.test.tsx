ef72d7f56124ccc330bb83597d961c3919a98037
victor90braz <victor.90braz@gmail.com>
2022-06-28 13:54:17 +0200

    Updated the description about the HeaderComponent test

--- src/components/HeaderComponent/HeaderComponent.test.tsx
+++ src/components/HeaderComponent/HeaderComponent.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import HeaderComponent from "./HeaderComponent";
 
 describe("Given a HeaderComponent", () => {
-  describe("When its called to be rendered", () => {
+  describe("When it's invoked", () => {
     test("Then it should create a HeaderComponent with a tittle 'Volley World'", () => {
       const expectedText = "Volley World";
 
@@ -10,6 +10,13 @@ describe("Given a HeaderComponent", () => {
 
       const result = screen.getByRole("heading", { level: 1 });
       expect(result.textContent).toBe(expectedText);
+    });
+
+    test("Then it should create a HeaderComponent with an element 'Button'", () => {
+      render(<HeaderComponent />);
+
+      const elementButton = screen.getByRole("button");
+      expect(elementButton).toBeInTheDocument();
     });
   });
 });
